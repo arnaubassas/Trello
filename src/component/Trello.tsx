068600656c0ecfc03e3640293bb46625943509bc
arnaubassas <arnau_b17@hotmail.com>
2023-08-31 15:54:29 +0200

    Clean project libraries

---
+++ src/component/Trello.tsx
@@ -0,0 +1,21 @@
+
+
+export const Trello = () => {
+
+
+
+    return (
+        <>
+            <div className="toDoList">
+                <div className="topList">
+                    <h5>Lista de tareas</h5>
+                    <button className="optionsList">...</button>
+                </div>
+                <div className="botList">
+                    <button className="listBotton">+ Añada una tarjeta </button>
+                </div>
+
+            </div>
+        </>
+    )
+}
